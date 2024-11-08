b7a22391cf19c2e06793ddd114777a80daa68adb
tad dy <a@a.com>
2024-11-08 17:10:29 -0500

    final tweaks and storing READMEs/pt_8.md

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ from helpers.send_email import send_email
 import agentops
 agentops.init(os.getenv("AGENTOPS_API_KEY"))
 
+emails=(os.getenv("COMMA_SEPARATED_EMAILS") or "")
+
 # vvv YAML Configuration vvv
 current_date = datetime.now().strftime("%Y-%m-%d") # Include current date for context
 replacements = {
@@ -75,7 +77,7 @@ def main():
     print('FINAL OUTPUT')
     print(crew_output.raw)
 
-    email_list = ["tad@cmdlabs.io"]
+    email_list = emails.split(",")
     for email in email_list:
         if bool(email) and is_valid_email(email):
             send_email([email.strip()], format_news_for_email(crew_output.pydantic, current_date))
